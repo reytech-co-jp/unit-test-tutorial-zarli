26643aa903fab620be0c3f322eececb7c869367d
zar li <zarlihnin@reytech.co.jp>
2022-10-27 15:02:35 +0900

    feat: Controllerのテスト追加

---
+++ src/test/java/com/example/demo/controller/AnimeControllerTests.java
@@ -0,0 +1,35 @@
+package com.example.demo.controller;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ExtendWith(SpringExtension.class)
+class AnimeControllerTests {
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void getAllAnime() throws Exception {
+
+        String url = "/api/anime";
+        MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(url)
+                .accept(MediaType.APPLICATION_JSON_VALUE)).andReturn();
+
+        int status = mvcResult.getResponse().getStatus();
+
+        assertEquals(400, status);
+    }
+
+}

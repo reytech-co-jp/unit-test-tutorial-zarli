77a87f3a4cc31be923f05db47a8eecd2f1402d41
zar li <zarlihnin@reytech.co.jp>
2022-11-16 17:22:29 +0900

    fix: 変数名の変更

--- src/test/java/com/example/demo/mapper/AnimeMapperTests.java
+++ src/test/java/com/example/demo/mapper/AnimeMapperTests.java
@@ -25,29 +25,29 @@ class AnimeMapperTests {
     @DataSet(value = "anime.yml")
     void アニメが全件取得できること() {
         List<Anime> animeList = Arrays.asList(new Anime(1, "Anime1", "Action"), new Anime(2, "Anime2", "Adventure"));
-        List<Anime> expectedAnimeList = animeMapper.findAll();
-        assertThat(expectedAnimeList).hasSize(2).containsAll(animeList);
+        List<Anime> actualAnimeResult = animeMapper.findAll();
+        assertThat(actualAnimeResult).hasSize(2).containsAll(animeList);
 
     }
 
     @Test
     @DataSet(value = "empty.yml")
     void アニメが空になること() {
-        List<Anime> animeList = animeMapper.findAll();
-        assertThat(animeList).isEmpty();
+        List<Anime> actualAnimeResult = animeMapper.findAll();
+        assertThat(actualAnimeResult).isEmpty();
     }
 
     @Test
     @DataSet(value = "anime.yml")
     void 引数のidでアニメを取得できること() {
-        Optional<Anime> anime = animeMapper.findById(1);
-        assertThat(anime).contains(new Anime(1, "Anime1", "Action"));
+        Optional<Anime> actualAnimeResult = animeMapper.findById(1);
+        assertThat(actualAnimeResult).contains(new Anime(1, "Anime1", "Action"));
     }
 
     @Test
     @DataSet(value = "empty.yml")
     void 引数のidに対したアニメが存在しない時_空のOptionalを取得すること() {
-        Optional<Anime> anime = animeMapper.findById(3);
-        assertThat(anime).isEmpty();
+        Optional<Anime> actualAnimeResult = animeMapper.findById(3);
+        assertThat(actualAnimeResult).isEmpty();
     }
 }

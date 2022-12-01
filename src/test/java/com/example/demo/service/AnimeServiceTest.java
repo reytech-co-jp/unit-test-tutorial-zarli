8cb1fbfad84bf7b15cde192dcd89ec39624acbe4
zar li <zarlihnin@reytech.co.jp>
2022-12-02 00:50:38 +0900

    fix: 削除メソッド名の修正

--- src/test/java/com/example/demo/service/AnimeServiceTest.java
+++ src/test/java/com/example/demo/service/AnimeServiceTest.java
@@ -79,7 +79,7 @@ class AnimeServiceTest {
     }
 
     @Test
-    public void アニメが空で削除するときに例外をthrowすること() {
+    public void 削除象のアニメが存在しないときに例外をthrowすること() {
         doReturn(Optional.empty()).when(animeMapper).findById(1);
         assertThatThrownBy(() -> animeService.deleteAnime(1))
                 .isInstanceOf(ResourceNotFoundException.class)

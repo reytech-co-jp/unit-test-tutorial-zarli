c91e580b61d9b003f258c4427a295a516bd5c279
zar li <zarlihnin@reytech.co.jp>
2022-12-07 01:41:44 +0900

    feat: controllerテストの追加

--- src/test/java/com/example/demo/controller/AnimeControllerTests.java
+++ src/test/java/com/example/demo/controller/AnimeControllerTests.java
@@ -1,34 +1,171 @@
 package com.example.demo.controller;
 
+import com.example.demo.entity.Anime;
+import com.example.demo.exception.ResourceNotFoundException;
+import com.example.demo.service.AnimeService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.util.StreamUtils;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+import static org.mockito.Mockito.doThrow;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@ExtendWith(SpringExtension.class)
 class AnimeControllerTests {
     @Autowired
-    private MockMvc mvc;
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ResourceLoader resourceLoader;
+
+    private final String BASE_PATH = "classpath:test-data/";
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockBean
+    AnimeService animeService;
 
     @Test
-    public void getAllAnime() throws Exception {
+    void アニメが全件取得できること() throws Exception {
+        var response = List.of(new Anime(1, "鬼滅の刃", "ダークファンタジー"), new Anime(2, "SPY×FAMILY", "ホームコメディ"), new Anime(3, "Dr.STONE", "survival"));
 
         String url = "/api/anime";
-        MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(url)
-                .accept(MediaType.APPLICATION_JSON_VALUE)).andReturn();
+        Mockito.when(animeService.getAllAnime()).thenReturn(response);
+        var result = mockMvc.perform(get(String.format("/api/anime"))
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk()).andReturn().getResponse().getContentAsString(StandardCharsets.UTF_8);
 
-        int status = mvcResult.getResponse().getStatus();
+        Assertions.assertEquals(objectMapper.readTree(getJsonFileData("200AllAnime.json")), objectMapper.readTree(result));
+    }
 
-        assertEquals(200, status);
+    @Test
+    void アニメが取得できるときに1件アニメを返すこと() throws Exception {
+        var id = 1;
+        var response = new Anime(1, "鬼滅の刃", "ダークファンタジー");
+
+        Mockito.when(animeService.getAnime(id)).thenReturn(response);
+        var result = mockMvc.perform(get(String.format("/api/anime/%d", id))
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk()).andReturn().getResponse().getContentAsString(StandardCharsets.UTF_8);
+
+        Assertions.assertEquals(objectMapper.readTree(getJsonFileData("200OneAnime.json")), objectMapper.readTree(result));
+    }
+
+    @Test
+    void アニメが取得できないときに例外をthrowすること() throws Exception {
+        var id = 4;
+
+        Mockito.when(animeService.getAnime(id)).thenThrow(
+                new ResourceNotFoundException("resource not found"));
+        var result = mockMvc.perform(get(String.format("/api/anime/%d", id))
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound())
+                .andReturn()
+                .getResolvedException().getMessage();
+
+        Assertions.assertEquals(result, "resource not found");
+    }
+
+    @Test
+    void アニメの登録ができること() throws Exception {
+        Anime anime = new Anime("Your Name", "Romantic Fantasy");
+
+        var result = mockMvc.perform(post(String.format("/api/anime/"))
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(this.objectMapper.writeValueAsString(anime)))
+                .andExpect(status().isCreated()).andReturn().getResponse().getContentAsString(StandardCharsets.UTF_8);
+
+        Assertions.assertEquals("anime successfully created", result);
+    }
+
+    @Test
+    void アニメの更新ができること() throws Exception {
+        var id = 1;
+        Anime anime = new Anime(id, "Your Name", "Romantic Fantasy");
+
+        var result = mockMvc.perform(patch(String.format("/api/anime/%d", id))
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(this.objectMapper.writeValueAsString(anime)))
+                .andExpect(status().isCreated()).andReturn().getResponse().getContentAsString(StandardCharsets.UTF_8);
+
+        Assertions.assertEquals("anime successfully updated", result);
+    }
+
+    @Test
+    void アニメの削除ができること() throws Exception {
+        var id = 1;
+        Anime anime = new Anime(id, "Your Name", "Romantic Fantasy");
+
+        var result = mockMvc.perform(delete(String.format("/api/anime/%d", id))
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(this.objectMapper.writeValueAsString(anime)))
+                .andExpect(status().isCreated()).andReturn().getResponse().getContentAsString(StandardCharsets.UTF_8);
+
+        Assertions.assertEquals("anime successfully deleted", result);
+    }
+
+    @Test
+    void 更新対象のアニメが存在しないときにレスポンスボディにエラーメッセージが返されること() throws Exception {
+        var id = 4;
+        var name = "Your Name";
+        var genre = "Romantic Fantasy";
+
+        Anime anime = new Anime(id, name, genre);
+
+        doThrow(new ResourceNotFoundException("resource not found")).when(animeService).updateAnime(id, name, genre);
+        var result = mockMvc.perform(patch(String.format("/api/anime/%d", id))
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(this.objectMapper.writeValueAsString(anime)))
+                .andExpect(status().isNotFound())
+                .andReturn()
+                .getResolvedException().getMessage();
+
+        Assertions.assertEquals(result, "resource not found");
+    }
+
+    @Test
+    void 削除対象のアニメが存在しないときにレスポンスボディにエラーメッセージが返されること() throws Exception {
+        var id = 4;
+
+        doThrow(new ResourceNotFoundException("resource not found")).when(animeService).deleteAnime(id);
+        var result = mockMvc.perform(delete(String.format("/api/anime/%d", id))
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound())
+                .andReturn()
+                .getResolvedException().getMessage();
+
+        Assertions.assertEquals(result, "resource not found");
+    }
+
+    private String getJsonFileData(String fileName) throws IOException {
+        var jsonResult = resourceLoader.getResource(BASE_PATH + fileName);
+        return StreamUtils.copyToString(jsonResult.getInputStream(), StandardCharsets.UTF_8);
     }
 }
